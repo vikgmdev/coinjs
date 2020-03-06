7fecafb64595af800dd19a1b0c5948007fea5308
Victor Garcia <vikgm.dev@gmail.com>
2020-03-06 13:50:33 -0600

    Initialize a FullNode

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import { FullNode } from './node';
 const node = new FullNode();
 
 (async () => {
-    await node.ensure();
-    await node.open();
     await node.connect();
-    node.startSync();
 })().catch(err => {
     console.error(err.stack);
     process.exit(1);
