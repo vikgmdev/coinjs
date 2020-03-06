862965843dfe12afb86490486c45bca4a57a76a9
Victor Garcia <vikgm.dev@gmail.com>
2020-03-06 13:48:50 -0600

    Implement P2P pool class

--- src/net/pool.ts
+++ src/net/pool.ts
@@ -1,12 +1,20 @@
-import { EventEmitter } from "events";
+import { EventEmitter } from 'events';
 import { Server, Socket, tcp } from '../wrappers';
 
 export class Pool extends EventEmitter {
-
     server: Server;
+    connected: boolean;
+
+    options = {
+        host: 'localhost',
+        maxInbound: 20,
+        port: 65378,
+    };
 
     constructor() {
         super();
+
+        this.connected = false;
 
         this.server = tcp.createServer();
 
@@ -19,12 +27,6 @@ export class Pool extends EventEmitter {
         });
 
         this.server.on('connection', (socket: Socket) => {
-            try {
-                this.handleSocket(socket, false);
-            } catch(e) {
-                this.emit('error', e);
-                return;
-            }
             this.emit('connection', socket);
         });
 
@@ -33,5 +35,34 @@ export class Pool extends EventEmitter {
             console.info(`Pool server listening on ${data.address} (port=${data.port}).`);
             this.emit('listening', data);
         });
+    }
+
+    /**
+     * Connect to the network.
+     */
+    async connect(): Promise<any> {
+        return await this._connect();
+    }
+
+    /**
+     * Connect to the network (no lock).
+     */
+    async _connect(): Promise<any> {
+        if (this.connected) return;
+
+        await this.listen();
+
+        this.connected = true;
+    }
+
+    /**
+     * Start listening on a server socket.
+     */
+    private async listen(): Promise<any> {
+        if (this.connected) throw new Error('Already listening.');
+
+        this.server.maxConnections = this.options.maxInbound;
+
+        await this.server.listen(this.options.port, this.options.host);
     }
 }
