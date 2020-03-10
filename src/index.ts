5cf96a61a11b0f99d0de75c9df6f60d9d54b7407
Victor Garcia <vikgm.dev@gmail.com>
2020-03-10 17:25:13 -0600

    Fix linter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import { FullNode } from './node';
 
-const node = new FullNode();
+const port = Number(process.argv[2]);
+const node = new FullNode(port);
 
-(async () => {
+(async (): Promise<void> => {
     await node.connect();
 })().catch(err => {
     console.error(err.stack);
     process.exit(1);
 });
+
+// process.stdin.on('data', (data) => {
+//     const nodes = node.pool.peers.list.toArray().map( item => ({
+//         prev: item.prev?.value.address.hostname,
+//         value: item.value?.address.hostname,
+//         next: item.next?.value.address.hostname,
+//     }));
+//     console.log(nodes);
+// })
